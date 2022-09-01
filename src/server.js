a60f12993c73d9e31da1727b083239689bb52589
soyeonnnb <la28s5d@naver.com>
2022-09-01 16:34:41 +0900

    server 기초

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import express from "express";
+import morgan from "morgan"
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
+
+const home = (req, res) => {
     return res.send("Main page");
 };
-const handleLogin = (req, res) => {
-    return res.send("Login here");
-}
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+
+app.use(logger);
+app.get("/", home);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
