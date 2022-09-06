18e2ccbf341d9080ae7602f655f59960a4aa8e8f
soyeonnnb <la28s5d@naver.com>
2022-09-06 19:52:25 +0900

    server.js를 init.js와 server.js 로 분리

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 
@@ -7,8 +5,6 @@ import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -23,7 +19,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
