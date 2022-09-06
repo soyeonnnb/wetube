fd0f9d09b925a4a2e06d9ba9623506ddc7cb00bf
soyeonnnb <la28s5d@naver.com>
2022-09-06 19:21:49 +0900

    mongodb 설치 및 연결

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 
@@ -15,6 +16,8 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
