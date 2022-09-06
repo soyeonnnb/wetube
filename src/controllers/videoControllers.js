9d38454919a374efa665914ceebd37a23a0b3810
soyeonnnb <la28s5d@naver.com>
2022-09-06 19:53:46 +0900

    fake data삭제

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,29 +1,3 @@
-const videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 4,
-    comments: 3,
-    createdAt: "10 minutes ago",
-    views: 108,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 2,
-    comments: 1,
-    createdAt: "20 minutes ago",
-    views: 210,
-    id: 3,
-  },
-];
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
@@ -31,33 +5,21 @@ export const search = (req, res) => res.send("Search");
 
 export const watch = (req, res) => {
   const id = req.params.id;
-  const video = videos[id - 1];
   res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const getEdit = (req, res) => {
   const id = req.params.id;
-  const video = videos[id - 1];
   res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
   const id = req.params.id;
   const title = req.body.title;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = (req, res) => {
-  const newVideo = {
-    title: req.body.title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
 export const remove = (req, res) => res.send("Remove Videos");
