d90ab1921966509e7c6c145a4c5ac5dbea8cca20
soyeonnnb <la28s5d@naver.com>
2022-09-06 19:21:31 +0900

    edit upload 페이지(only template) 생성

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,35 +1,63 @@
+const videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 4,
+    comments: 3,
+    createdAt: "10 minutes ago",
+    views: 108,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 2,
+    comments: 1,
+    createdAt: "20 minutes ago",
+    views: 210,
+    id: 3,
+  },
+];
 export const home = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 4,
-      comments: 3,
-      createdAt: "10 minutes ago",
-      views: 108,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 2,
-      comments: 1,
-      createdAt: "20 minutes ago",
-      views: 210,
-      id: 3,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
 export const search = (req, res) => res.send("Search");
 
-export const see = (req, res) => res.send("see Videos");
-export const upload = (req, res) => res.send("Upload Videos");
+export const watch = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const id = req.params.id;
+  const title = req.body.title;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) => {
+  res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const newVideo = {
+    title: req.body.title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
 export const remove = (req, res) => res.send("Remove Videos");
-export const edit = (req, res) => res.send("Edit Videos");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { see, edit, upload, remove } from "../controllers/videoControllers.js";
+import {
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+  remove,
+} from "../controllers/videoControllers.js";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+
 videoRouter.get("/:id(\\d+)/remove", remove);
 
 export default videoRouter;
