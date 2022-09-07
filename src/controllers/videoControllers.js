abcd5d834311c4d19d0dafe1330cd9511d1a0881
soyeonnnb <la28s5d@naver.com>
2022-09-07 20:37:49 +0900

    post upload video

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,25 +1,41 @@
-export const home = (req, res) => {
+import Video from "../models/Video";
+export const home = async (req, res) => {
+  const videos = await Video.find({});
   res.render("home", { pageTitle: "Home", videos });
 };
 export const search = (req, res) => res.send("Search");
 
 export const watch = (req, res) => {
-  const id = req.params.id;
-  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  const { id } = req.params;
+  res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
 export const getEdit = (req, res) => {
-  const id = req.params.id;
-  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  const { id } = req.params;
+  res.render("edit", { pageTitle: `Editing: ${video.title}` });
 };
 export const postEdit = (req, res) => {
-  const id = req.params.id;
-  const title = req.body.title;
+  const { id } = req.params;
+  const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
   return res.redirect("/");
 };
 export const remove = (req, res) => res.send("Remove Videos");
