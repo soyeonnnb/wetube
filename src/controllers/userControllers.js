e2b5fd16f880cd9c696f2447541de717325cd3e1
soyeonnnb <la28s5d@naver.com>
2022-09-13 19:27:51 +0900

    비밀번호 확인, 이메일/유저네임 중복 확인

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,8 +1,22 @@
+import { render } from "pug";
 import User from "../models/User.js";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, email, username, password, location } = req.body;
+  const { name, email, username, password, password2, location } = req.body;
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "비밀번호가 일치하지 않습니다.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "이미 존재하는 닉네임/이메일 입니다.",
+    });
+  }
   await User.create({
     name,
     email,
