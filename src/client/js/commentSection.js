342fdfd9d2f75c89fc79abd3e39b69054b552b05
soyeonnnb <la28s5d@naver.com>
2022-09-29 23:42:46 +0900

    comment 실시간으로 보이기

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,24 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+const addComment = (text, id) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  newComment.dataset.id = id;
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  const span2 = document.createElement("span");
+  span2.innerText = ` ❌`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  newComment.appendChild(span2);
+  videoComments.prepend(newComment);
+};
+
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
@@ -9,13 +26,17 @@ const handleSubmit = (event) => {
   if (text === "") {
     return;
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
+  if (response.status === 201) {
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
+  }
   textarea.value = "";
 };
 
